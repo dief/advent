3020e387e364473218037882748a2cada471069d
David Charles Pollack <dcpollack@gmail.com>
2024-12-17 20:26:44 -0500

    Java 2024 day 13 code streamlined

--- java/2024/src/main/java/com/leynmaster/advent/aoc2024/day13/ClawMachine.java
+++ java/2024/src/main/java/com/leynmaster/advent/aoc2024/day13/ClawMachine.java
@@ -1,6 +1,5 @@
 package com.leynmaster.advent.aoc2024.day13;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,22 +25,14 @@ public class ClawMachine {
     }
 
     public long tokensNeeded() {
-        long detAll = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-        long detA = matrix[0][2] * matrix[1][1] - matrix[0][1] * matrix[1][2];
-        long detB = matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0];
-        long a = detA / detAll;
-        long b = detB / detAll;
-        long xResult = matrix[0][0] * a + matrix[0][1] * b;
-        long yResult = matrix[1][0] * a + matrix[1][1] * b;
-        if (xResult == matrix[0][2] && yResult == matrix[1][2]) {
+        long determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        long a = (matrix[0][2] * matrix[1][1] - matrix[0][1] * matrix[1][2]) / determinant;
+        long b = (matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0]) / determinant;
+        if (matrix[0][0] * a + matrix[0][1] * b == matrix[0][2]
+                && matrix[1][0] * a + matrix[1][1] * b == matrix[1][2]) {
             return a * 3 + b;
         }
         return 0;
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.deepToString(matrix);
     }
 
     private void setColumn(String line, int column) {
