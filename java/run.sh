42ab122beb00509e0cb1ba98d2075ba02cc54b8f
David Charles Pollack <dcpollack@gmail.com>
2025-10-11 20:05:15 -0400

    Java build streamlining

--- java/run.sh
+++ java/run.sh
@@ -7,5 +7,4 @@ $HOME/.m2/repository/ch/qos/logback/logback-classic/1.5.12/logback-classic-1.5.1
 $HOME/.m2/repository/ch/qos/logback/logback-core/1.5.12/logback-core-1.5.12.jar:\
 $HOME/.m2/repository/org/slf4j/slf4j-api/2.0.15/slf4j-api-2.0.15.jar
 
-exec java --enable-preview -Xlog:gc+heap+exit \
-com.leynmaster.advent.aoc$1.day$2.Day$2
+exec java -Xlog:gc com.leynmaster.advent.aoc$1.day$2.Day$2
