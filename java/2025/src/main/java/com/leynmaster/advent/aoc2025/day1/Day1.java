b389d8025d4b608ae1e24c5601c6a3919325279e
David Charles Pollack <dcpollack@gmail.com>
2025-12-02 23:15:16 -0500

    Java 2025 day 1 optimization

--- java/2025/src/main/java/com/leynmaster/advent/aoc2025/day1/Day1.java
+++ java/2025/src/main/java/com/leynmaster/advent/aoc2025/day1/Day1.java
@@ -13,27 +13,26 @@ public class Day1 {
     private static final String INPUT = "../../inputs/2025/day1/input.txt";
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private int zeroCount = 0;
-    private int pastZeroCount = 0;
+    private int zeroClickCount = 0;
 
     void main() throws IOException {
         logger.info("Starting");
         int dial = 50;
         for (String instruction : FileUtils.readLines(new File(INPUT), StandardCharsets.UTF_8)) {
-            char direction = instruction.charAt(0);
             int amount = reduce(Integer.parseInt(instruction.substring(1)));
-            dial = direction == 'R' ? spinRight(dial, amount) : spinLeft(dial, amount);
+            dial = instruction.charAt(0) == 'R' ? spinRight(dial, amount) : spinLeft(dial, amount);
             if (dial == 0) {
                 zeroCount++;
             }
         }
         logger.info("Part 1: {}", zeroCount);
-        logger.info("Part 2: {}", pastZeroCount);
+        logger.info("Part 2: {}", zeroClickCount);
     }
 
     private int spinRight(int current, int amount) {
         int next = current + amount;
         if (next > 99) {
-            pastZeroCount++;
+            zeroClickCount++;
             return next - 100;
         }
         return next;
@@ -43,19 +42,19 @@ public class Day1 {
         int next = current - amount;
         if (next < 0) {
             if (current != 0) {
-                pastZeroCount++;
+                zeroClickCount++;
             }
-            return 100 + next;
+            return next + 100;
         }
         if (next == 0) {
-            pastZeroCount++;
+            zeroClickCount++;
         }
         return next;
     }
 
     private int reduce(int amount) {
         int hundreds = amount / 100;
-        pastZeroCount += hundreds;
+        zeroClickCount += hundreds;
         return amount - 100 * hundreds;
     }
 }
