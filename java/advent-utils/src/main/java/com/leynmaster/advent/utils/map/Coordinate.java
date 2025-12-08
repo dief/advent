478cacbe4bc9a00742e6d9d7944997eb805bcb05
David Charles Pollack <dcpollack@gmail.com>
2025-12-07 22:17:13 -0500

    Default equals and hashCode for record type

--- java/advent-utils/src/main/java/com/leynmaster/advent/utils/map/Coordinate.java
+++ java/advent-utils/src/main/java/com/leynmaster/advent/utils/map/Coordinate.java
@@ -13,20 +13,6 @@ public record Coordinate(int x, int y) {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Coordinate that = (Coordinate)o;
-        return x == that.x && y == that.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
     public String toString() {
         return "[" + x + ", " + y + "]";
     }
